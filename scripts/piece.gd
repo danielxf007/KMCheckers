29b801f01fa85f2c56e1eb700b82ffdc75f7562b
danielxf007 <daniel.higuita007@gmail.com>
2021-10-23 17:17:51 -0500

    piece logic implemented

--- scripts/piece.gd
+++ scripts/piece.gd
@@ -1,25 +1,22 @@
 extends AnimatedSprite
 
 class_name Piece
-enum {KNIGHT, MONSTER, KING_KNIGHT, KING_MONSTER} #Piece states
+enum {KNIGHT, MONSTER, KING_KNIGHT, KING_MONSTER} #Piece types
 enum {NORMAL_COLOR, KING_COLOR, SELECTED_COLOR}  #Color states
-const KING_MASK: int = 0x10
+const ID_BITS: int = 5
+const ID_MASK: int = 0x1F
+const KING_MASK: int = 0x40
 const FRAMES_PATH: String = "res://animations/piece_anim.tres"
 const COLORS: Array = ["ffffff", "dfc0ff00", "3dffffff"]
 const ANIMATIONS: Array = ["KnightAnim", "MonsterAnim"]
 
-var init_piece_state: int
-var curr_piece_state: int
-var curr_color_state: int
+var piece_state: int #piece state [pt1, pt0, id4, id3, id2, id1, id0]
 var selected: bool
 
-func init(_init_piece_state: int, pos: Vector2) -> void:
+func init(id: int, pos: Vector2) -> void:
 	self.frames = load(self.FRAMES_PATH)
-	self.init_piece_state = _init_piece_state
-	self.curr_piece_state = _init_piece_state
+	self.piece_state = id
 	self.selected = false
-	self.animation = self.ANIMATIONS[_init_piece_state]
-	self.curr_color_state = NORMAL_COLOR
 	self.global_position = pos
 
 func activate() -> void:
@@ -31,9 +28,11 @@ func desactivate() -> void:
 	self.selected = false
 	self.hide()
 
+func set_type(type: int) -> void:
+	self.piece_state &= self.ID_MASK
+	self.piece_state |= (type<<self.ID_BITS)
+
 func reset() -> void:
-	self.curr_piece_state = self.init_piece_state
-	self.curr_color_state = NORMAL_COLOR
 	self.modulate = self.COLORS[NORMAL_COLOR]
 	self.selected = false
 
@@ -41,11 +40,11 @@ func move(pos: Vector2) -> void:
 	self.global_position = pos
 
 func is_king() -> bool:
-	return bool(self.curr_piece_state & self.KING_MASK)
+	return bool(self.piece_state & self.KING_MASK)
 
 func promote() -> void:
 	self.curr_piece_state |= self.KING_MASK
-	self.modulate = self.COLORS[self.KING]
+	self.modulate = self.COLORS[KING_COLOR]
 
 func is_selected() -> bool:
 	return self.selected
