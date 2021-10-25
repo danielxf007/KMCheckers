e43ccf8b5085d9b2599913f0815f6ace755d4d87
danielxf007 <daniel.higuita007@gmail.com>
2021-10-25 10:57:16 -0500

    player id added

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,6 +1,7 @@
 extends Node2D
 signal timers_were_set(clk_time)
 enum {KNIGHT, MONSTER, KING_KNIGHT, KING_MONSTER, EMPTY=-1}
+enum {PLAYER_1_ID, PLAYER_2_ID}
 const PLAYER_1_PIECES_INIT_POS: Vector2 = Vector2(32, 32)
 const PLAYER_2_PIECES_INIT_POS: Vector2 = Vector2(32, 352)
 const CELL_DIMENSIONS: Vector2 = Vector2(64, 64)
@@ -43,19 +44,21 @@ func place_piece(n_rows: int, row_offset: int, init_col_pos: int, init_pos: Vect
 			self.piece_id+=1
 		col_pos = (col_pos+1)%2
 
-func set_game_pices(type: int, n_rows: int, row_offset: int, init_col_pos: int) -> void:
+func set_game_pices(type: int, player_id: int, n_rows: int,
+row_offset: int, init_col_pos: int) -> void:
 	var col_pos: int = init_col_pos
 	for i in range(n_rows):
 		for j in range(col_pos, int(self.BOARD_DIMENSIONS.x), 2):
 			self.game_board[i+row_offset][j].set_type(type)
+			self.game_board[i+row_offset][j].set_player_id(player_id)
 			self.game_board[i+row_offset][j].activate()
 		col_pos = (col_pos+1)%2
 
 func _on_GameSetUpUI_game_started(game_setup_data: Dictionary) -> void:
 	var player_1_piece_type: int = randi()%2
-	self.set_game_pices(player_1_piece_type, 3, 0, 1)
+	self.set_game_pices(player_1_piece_type, PLAYER_1_ID, 3, 0, 1)
 	var player_2_piece_type: int = (player_1_piece_type+1)%2
-	self.set_game_pices(player_2_piece_type, 3, 5, 0)
+	self.set_game_pices(player_2_piece_type, PLAYER_2_ID, 3, 5, 0)
 	if game_setup_data["use_clk"]:
 		self.timers["p_1"].wait_time = game_setup_data["clk_time"]
 		self.timers["p_2"].wait_time = game_setup_data["clk_time"]
