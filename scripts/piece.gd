16aaf96af4ae2e0bbfe9a210e76cdf5db0ce105a
danielxf007 <daniel.higuita007@gmail.com>
2021-10-25 10:56:24 -0500

    added a player bit

--- scripts/piece.gd
+++ scripts/piece.gd
@@ -3,22 +3,27 @@ extends AnimatedSprite
 class_name Piece
 enum {KNIGHT, MONSTER, KING_KNIGHT, KING_MONSTER} #Piece types
 enum {NORMAL_COLOR, KING_COLOR, SELECTED_COLOR}  #Color types
-const ID_BITS: int = 5
-const ID_MASK: int = 0x1F
-const KING_MASK: int = 0x40
+enum {PLAYER_ID_MASK=0x80, KING_MASK=0x40, PIECE_TYPE_MASK=0x20, ID_MASK=0x1F} #Masks
+enum {PIECE_TYPE_BIT=5, PLAYER_ID_BIT=8} #Start bit positions
 const FRAMES_PATH: String = "res://animations/piece_anim.tres"
 const COLORS: Array = ["ffffff", "dfc0ff00", "3dffffff"]
 const ANIMATIONS: Array = ["KnightAnim", "MonsterAnim"]
 
-var piece_state: int #piece state [pt1, pt0, id4, id3, id2, id1, id0]
+var piece_state: int #piece state [pl, k, pt, id4, id3, id2, id1, id0]
 var selected: bool
 
 func init(id: int, pos: Vector2) -> void:
 	self.frames = load(self.FRAMES_PATH)
-	self.piece_state = id
+	self.piece_state = id 
 	self.selected = false
 	self.global_position = pos
 	self.hide()
+
+func get_id() -> int:
+	return self.piece_state & ID_MASK
+
+func get_player_id() -> int:
+	return self.piece_state & PLAYER_ID_MASK
 
 func activate() -> void:
 	self.playing = true
@@ -30,11 +35,15 @@ func desactivate() -> void:
 	self.hide()
 
 func set_type(type: int) -> void:
-	self.piece_state &= self.ID_MASK
-	self.piece_state |= (type<<self.ID_BITS)
+	self.piece_state &= (ID_MASK | PLAYER_ID_MASK)
+	self.piece_state |= (type<<PIECE_TYPE_BIT)
 	self.play(self.ANIMATIONS[type])
 
+func set_player_id(player_id: int) -> void:
+	self.piece_state |= (player_id<<PLAYER_ID_BIT)
+
 func reset() -> void:
+	self.piece_state &= (PIECE_TYPE_MASK| PLAYER_ID_MASK | ID_MASK)
 	self.modulate = self.COLORS[NORMAL_COLOR]
 	self.selected = false
 
@@ -42,10 +51,10 @@ func move(pos: Vector2) -> void:
 	self.global_position = pos
 
 func is_king() -> bool:
-	return bool(self.piece_state & self.KING_MASK)
+	return bool(self.piece_state & KING_MASK)
 
 func promote() -> void:
-	self.curr_piece_state |= self.KING_MASK
+	self.curr_piece_state |= KING_MASK
 	self.modulate = self.COLORS[KING_COLOR]
 
 func is_selected() -> bool:
